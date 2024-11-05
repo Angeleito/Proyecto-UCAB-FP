fa358979a7a0341c795321be7f36e50b8237071e
Angeleito <angelgap1112@gmail.com>
2024-11-04 20:58:02 -0400

    Update Proyect.py
    4/11/2024

--- Proyect.py
+++ Proyect.py
@@ -1,46 +1,55 @@
 def main():
-    c = 0
-    print("Bienvenido al programa, ingrese(A): Para calcular su deuda, ingrese(S): para salir del programa:")
+  c = 0
+  while True:
+    print("Bienvenido al programa para calcular su pago minimo de deuda, ingrese (A): Para calcular su deuda, ingrese (S): para salir del programa:")
     ini = input()
-    if ini == "A":
-        while (c < 3):
-            print("Bienvenido, por favor ingrese los siguientes datos:")
-            print("Ingresa tus iniciales(Preferiblemente de 4 letras):")
-            I = input()
-            print("Ingrese la clave correspondiente a un numero entero positivo de 4 digitos:")
-            C = int(input())
-            print("Ingrese la deuda correspondiente a un numero entero positivo de 4 o 5 digitos:")
-            D = int(input())
-        
-            # Validar iniciales
-            if(len(I) != 7):
-                print("Las iniciales son: ", I)
-            else:
-                print("Los datos de las iniciales no son validos")
+    if (ini == "S"):
+      print("Saliendo del programa")
+      break
+    elif (ini == "A"):
+      while c < 3:
+        print("Bienvenido, por favor ingrese los siguientes datos:")
+        print("Ingresa en mayusculas tus iniciales(Preferiblemente de 4 letras):")
+        I = input()
+        if not (len(I) != 2 and len(I) != 6):
+          print("Los datos de las iniciales no son válidos")
+          continue
 
-            # Validar clave
-            if(C >= 1000) and (C <= 9999):
-                print("La clave es: ", C)
-            else:
-                print("La clave ingresada no es valida")
-        
-            # Validar deuda
-            if(D >= 1000) and (D <= 99999):
-                print("La deuda es: ",D,"BS")
-            else:
-                print("La deuda ingresada no es valida")
-            
-            # Calcular pago minimo de deuda
-            MI = ((30 * D) / 100)
-            DI = (D + MI)
-            PM = (DI / 12)
+        print("Ingrese la clave correspondiente a un numero entero positivo de 4 digitos:")
+        C = int(input())
+        if(C >= 1000) and (C <= 9999):
+          print("La clave es:",C)
+        else:
+          print("La clave ingresada no es valida")
 
-            print("La tasa de interes fija es del 30%")
-            print("La deuda con el monto de interes anual es: ", DI,"BS")
-            print("El pago minimo es: ", PM,"BS")
+        print("Ingrese la deuda correspondiente a un numero entero positivo de 4 o 5 digitos:")
+        D = int(input())
+        if(D >= 1000) and (D <= 99999):
+          print("La deuda es:",D,"BS")
+        else:
+          print("La deuda ingresada no es valida")
+
+        print("Las iniciales son:", I)
+        print("La clave es:", C)
+        print("La deuda es:", D, "BS")
+
+        # Calcular pago mínimo de deuda
+        MI = ((30 * D) / 100)
+        DI = (D + MI)
+        PM = (DI / 12)
+
+        print("La tasa de interés fija es del 30%")
+        print("La deuda con el monto de interés es:", DI, "BS")
+        print("El pago mínimo es:", PM, "BS")
+
+        print("¿Desea realizar otro cálculo? (S/N)")
+        resp = input()
+        if (resp != "S"):
+          print("Saliendo del programa")
+          c = 4
+          break
+
+        c += 1
     else:
-        print("Saliendo del programa")
-        
-        
-        c+=1
+      print("Opción no válida, intente de nuevo.")
 main()
